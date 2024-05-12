390c13384c3b8c0a3fa7e251f78aedcb9975c0ca
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-05-12 18:12:23 +0600

    one js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        app: "./src/main.jsx",
+      },
+    },
+  },
+  base: "https://cdn.jsdelivr.net/gh/Jisan-mia/logo-prticles/dist/",
+});
